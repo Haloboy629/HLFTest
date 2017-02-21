07bcdd0928ebada36c07348e0e08d8e5875eaa1d
LiuPyfan <491100611@qq.com>
2017-02-21 09:26:08 +0800

    完成第二个Aty

--- HLFTest/app/src/main/java/com/example/pf/hlftest/MainActivity.java
+++ HLFTest/app/src/main/java/com/example/pf/hlftest/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.pf.hlftest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -11,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView testTv = (TextView) findViewById(R.id.test_tv);
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
     }
 }
